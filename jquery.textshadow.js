3ea009eac36aba2735220ed7f1f5be026fd5b54b
Grady Kuhnline <github@heygrady.net>
2012-05-03 17:57:36 -0700

    fixes #8 in IE7 and below. IE 8 and IE9 operate in a weird rendering mode when a transitional doctype is declared and the normal hasLayout hacks no longer apply to those browsers.

--- jquery.textshadow.js
+++ jquery.textshadow.js
@@ -147,16 +147,16 @@
         if (!text.length) { // IE 9
             return null;
         }
-        var shadow = shadowNode.cloneNode(),
-            orig = origNode.cloneNode(),
-            copy = copyNode.cloneNode();
+        var shadow = shadowNode.cloneNode(false),
+            orig = origNode.cloneNode(false),
+            copy = copyNode.cloneNode(false);
             
         shadow.appendChild(copy);
         shadow.appendChild(orig);
-        //shadow.appendChild(document.createTextNode(" ")); // fixes #8
         
         orig.appendChild(document.createTextNode(text));
         copy.appendChild(document.createTextNode(text));
+
         return shadow;
     }
     
